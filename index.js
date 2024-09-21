a9a8ec82bdf5f479e2697f92abe1c206c8d8aa63
Leolfc <147149946+Leolfc@users.noreply.github.com>
2024-09-20 21:54:03 -0300

    Adicionei funcionalidades no codigo e prevenção de erros futuros

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const key = "22756c456f82548c21632c85e3808f8b";
 const botao = document.querySelector(".botao-buscar");
 botao.addEventListener("click", cliqueNoBotao);
+const inputCidade = document.querySelector(".input-city");
+
+inputCidade.addEventListener("keypress", (evento) => {
+  if (evento.key === "Enter") {
+    cliqueNoBotao();
+  }
+});
 
 function cliqueNoBotao() {
-  const cidade = document.querySelector(".input-city").value;
-
+  const cidade = document.querySelector(".input-city").value.trim();
+  if (cidade === "" || !isNaN(cidade)) {
+    alert("Por favor, digite o nome de uma cidade!");
+    return;
+  }
   buscarCidadeNoServidor(cidade);
 }
+
 //!FUNÇÃO ASSINCRONA
 async function buscarCidadeNoServidor(cidade) {
   try {
     const dadosDoServidor = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`
     ).then((resposta) => resposta.json());
-    console.log(dadosDoServidor);
 
+    if (dadosDoServidor.cod === "404") {
+      alert("Cidade não encontrada");
+      return;
+    }
     function colocarDadosNatela(dadosDoServidor) {
       document.querySelector(".city").innerHTML =
         "Tempo em " + dadosDoServidor.name;
-      document.querySelector(".temp").innerHTML =(dadosDoServidor.main.temp - 273.15).toFixed(2) + " °C";
+      document.querySelector(".temp").innerHTML =
+        (dadosDoServidor.main.temp - 273.15).toFixed(2) + " °C";
       document.querySelector(".texto-previsao").innerHTML =
         dadosDoServidor.weather[0].description;
       document.querySelector(".texto-umidade").innerHTML =
